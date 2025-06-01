69bb90a8869a2bd046fb21449bd94a9bfb36f13f
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2025-06-01 03:26:01 +0200

    fix testts

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ['@1inch/eslint-config'],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    rules: {
-        // Add any project-specific rules here
-    },
-}; 
